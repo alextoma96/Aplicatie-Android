07600ef4200fb4338ca7732d22fd0aea62af25e3
idanciu <iulia.danciu@kepler-rominfo.com>
2017-09-15 18:35:19 +0300

    iulia

--- app/src/main/java/Commons/Utilizator.java
+++ app/src/main/java/Commons/Utilizator.java
@@ -5,4 +5,22 @@ package Commons;
  */
 
 public class Utilizator {
+    private String nume;
+    private String username;
+    private String parola;
+    private Angajat angajat;
+
+
+
+    public String getNume() { return nume; }
+    public Utilizator setNume(String nume) { this.nume = nume; return this;}
+
+    public String getUsername() { return username; }
+    public Utilizator setUsername(String username) { this.username = username; return this;}
+
+    public String getParola() { return parola; }
+    public Utilizator setParola(String parola) { this.parola = parola; return this; }
+
+    public Angajat getAngajat() { return angajat; }
+    public Utilizator setAngajat(Angajat angajat) { this.angajat = angajat; return this;}
 }
