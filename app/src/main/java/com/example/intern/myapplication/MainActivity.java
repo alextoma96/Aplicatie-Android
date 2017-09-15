2e7d1e78ab359ae8524b8fd6b9c8347caf4519b5
Andrei <stamate.cristian97@yahoo.com>
2017-09-15 16:54:39 +0300

    Andrei
    Andei

--- app/src/main/java/com/example/intern/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intern/myapplication/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         WebView homeText = (WebView) findViewById(R.id.homeText);
-        homeText.loadData(getResources().getString(R.string.homeTxt), "", "UTF-8");
+        homeText.loadData("<p style=\"text.align: justify\">" + getResources().getString(R.string.homeTxt) + "</p>", null, "UTF-8");
 
 
         initIP();
